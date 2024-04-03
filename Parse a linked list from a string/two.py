c2e2c5a698c8fb45a1b08c397304ea62f4d5ebe3
Oleksa <khita.pn@ucu.edu.ua>
2024-04-03 19:30:29 +0300

    Third problem solved

--- Parse a linked list from a string/two.py
+++ Parse a linked list from a string/two.py
@@ -1,6 +1,11 @@
-def linked_list_from_string(s):
-    node = None
-    for i in reversed(s.split(' -> ')):
-        if i != 'None':
-            node = Node(int(i), node)
+def push(head, data):
+    node = Node(data)
+    node.next = head
     return node
+
+def build_one_two_three():
+    head = None
+    head = push(head, 3)
+    head = push(head, 2)
+    head = push(head, 1)
+    return head
