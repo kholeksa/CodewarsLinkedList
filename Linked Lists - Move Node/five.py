f26d6c239c98d7c1b691688fde2a7311208982be
Oleksa <khita.pn@ucu.edu.ua>
2024-04-03 20:17:08 +0300

    5th problem solved

---
+++ Linked Lists - Move Node/five.py
@@ -0,0 +1,16 @@
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+    
+class Context(object):
+    def __init__(self, source, dest):
+        self.source = source
+        self.dest = dest
+    
+def move_node(source, dest):
+    if source is None:
+        raise ValueError
+    temp = source.next
+    source.next = dest
+    return Context(temp, source)
